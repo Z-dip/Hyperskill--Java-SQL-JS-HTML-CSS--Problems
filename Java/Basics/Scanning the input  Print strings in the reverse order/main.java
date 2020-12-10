9980071d2c58d3603579677ad9258a3fe10507be
Zdenko Filip <67583742+Z-dip@users.noreply.github.com>
2020-12-10 16:55:11 +0100

    Create main.java

---
+++ Java/Basics/Scanning the input  Print strings in the reverse order/main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+//put imports you need here
+
+class Main {
+    public static void main(String[] args) {
+        // put your code here
+        Scanner scanner = new Scanner(System.in);
+        String a = scanner.nextLine();
+        String b = scanner.nextLine();
+        String c = scanner.nextLine();
+    
+        System.out.println(c + "\n" + b + "\n" + a);
+    }
+}
