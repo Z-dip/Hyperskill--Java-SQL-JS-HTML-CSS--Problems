975db87f1e94c762eb52c2a0765a133462b9559f
Zdenko Filip <67583742+Z-dip@users.noreply.github.com>
2020-12-15 15:04:48 +0100

    Create main.java

---
+++ Java/Basics/Math library  Pow/main.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class Main {
+    // put your code here
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double num = scanner.nextDouble();
+        double num2 = scanner.nextDouble();
+        System.out.println(Math.pow(num, num2));
+        
+    }
+}
