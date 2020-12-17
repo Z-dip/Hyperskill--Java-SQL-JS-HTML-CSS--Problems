69969f83186e71e88ee15921a0c80918a3f0ae89
Zdenko Filip <67583742+Z-dip@users.noreply.github.com>
2020-12-17 15:06:10 +0100

    Create main.java

---
+++ Java/OOP/Retrieving Class instances  Find the Method/main.java
@@ -0,0 +1,18 @@
+
+import java.lang.reflect.Method;
+import java.util.*;
+
+
+class MethodFinder {
+
+    public static String findMethod(String methodName, String[] classNames) throws ClassNotFoundException {
+        for (String clsName : classNames) {
+            for(Method mth : Class.forName(clsName).getMethods()) {
+                if (methodName.equals(mth.getName())) {
+                    return clsName;
+                }
+            }
+        }
+        return null;
+    }
+}
