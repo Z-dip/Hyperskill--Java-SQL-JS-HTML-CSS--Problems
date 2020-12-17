af46f5a82550ae32d2fa92c20778290423b1303a
Zdenko Filip <67583742+Z-dip@users.noreply.github.com>
2020-12-17 14:39:15 +0100

    Create main.java

---
+++ Java/OOP/Instance methods  Circle/main.java
@@ -0,0 +1,14 @@
+class Circle {
+
+    double radius;
+    // write methods here
+
+    public double getLength() {
+        return (double) (2 * Math.PI * radius);
+    }
+    public double getArea() {
+        return (double) (Math.PI * (radius * radius));
+
+    }
+    
+}
