3eec9e394aa335e889ef5c82cecc7038e10a88de
Zdenko Filip <67583742+Z-dip@users.noreply.github.com>
2020-12-18 16:08:03 +0100

    Create main.java

---
+++ Java/Collections/ArrayList  Output the list's elements/main.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+    // write your code here
+        ArrayList<String> nameList = new ArrayList<>(Arrays.asList("Mr.Green", "Mr.Yellow", "Mr.Red"));
+        
+    }
+}
