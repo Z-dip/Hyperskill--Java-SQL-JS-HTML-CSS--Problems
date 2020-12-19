950e76e8ef92ca4f4a0bea8f2a5d9d2b24e55f10
Zdenko Filip <67583742+Z-dip@users.noreply.github.com>
2020-12-19 18:34:31 +0100

    Create main.java

---
+++ Java/RegeX/Vehicle registration number/main.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String regNum = scanner.nextLine(); // a valid or invalid registration number
+
+        String regValidate = "\\s*[ABEKMHOPCTYX][0-9]{3}[ABEKMHOPCTYX]{2}\\s*";
+        System.out.println(regNum.matches(regValidate));
+    }
+}
